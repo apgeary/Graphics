2e496c8fe0945fe3bbca6dd09d2ac16dbcb7f762
Alexander Geary <alexander.geary@yahoo.com>
2018-11-14 14:38:17 -0600

    Added two classes and modified existing

--- MyFrame.java
+++ MyFrame.java
@@ -1,10 +1,43 @@
+import javax.swing.JFrame;
 
-public class MyFrame {
-
+public class MyFrame extends JFrame
+{
+	
+	private final int WIDTH = 300;
+	private final int HEIGHT = 400;
+	private static MyPanel panel;
+	
+	public MyFrame(String title)
+	{
+		setTitle(title);
+		setSize(WIDTH, HEIGHT);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public void paintLine()
+	{
+		panel = new LineDemoPanel();
+		add(panel);
+		setVisible(true);
+	}
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		MyFrame frame  = new MyFrame("Line");
+		frame.paintLine();
+		
+		MyFrame frame1 = new MyFrame("Rectangle");
+		frame1.paintRectangle();
+	}
 
+	public void paintRectangle()
+	{
+		panel = new RectangleDemoPanel();
+		add(panel);
+		setVisible(true);
+		
+		
 	}
 
 }
